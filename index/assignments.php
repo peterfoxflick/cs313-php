7f42c88bc381ba6a018e6d0613e1a206eef86555
peterfoxflick <peterfoxflick@gmail.com>
2018-09-29 18:35:13 -0600

    Added Assignemnts page

--- index/assignments.php
+++ index/assignments.php
@@ -1,10 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <?php
-  echo file_get_contents('./Partials/header.php');
-  ?>
-
+  <?php include './Partials/header.php';?>
 </head>
 <body>
 
@@ -12,7 +9,20 @@
 echo "My first PHP script!";
 ?>
 
-<h1>Guess whos back</h1>
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <h1>Assignments</h1>
+      <div class="list-group">
+        <a href="./01-Teach/index.php" class="list-group-item list-group-item-action">01 Teach</a>
+        <a href="./02-Teach/index.html" class="list-group-item list-group-item-action">02 Teach</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<?php include './Partials/footer.php';?>
 
 </body>
 </html>
