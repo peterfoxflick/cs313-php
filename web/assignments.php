9f31917f527f8f7d2b47a6e87e3067c8c9309c6d
peterfoxflick <peterfoxflick@gmail.com>
2018-09-29 18:49:56 -0600

    Hold your horses

--- web/assignments.php
+++ web/assignments.php
@@ -6,17 +6,17 @@
 <body>
   <?php include './partials/nav.php';?>
 
-<div class="container">
-  <div class="row">
-    <div class="col">
-      <h1>Assignments</h1>
-      <div class="list-group">
-        <a href="./01-Teach/hello.html" class="list-group-item list-group-item-action">01 Teach</a>
-        <a href="./02-Teach/index.html" class="list-group-item list-group-item-action">02 Teach</a>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1>Assignments</h1>
+        <div class="list-group">
+          <a href="./01-Teach/hello.html" class="list-group-item list-group-item-action">01 Teach</a>
+          <a href="./02-Teach/index.html" class="list-group-item list-group-item-action">02 Teach</a>
+        </div>
       </div>
     </div>
   </div>
-</div>
 
 
 <?php include './partials/footer.php';?>
