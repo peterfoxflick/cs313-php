b223b90179f2090a942b604ce7e23673f837639f
peterfoxflick <peterfoxflick@gmail.com>
2018-09-29 18:42:24 -0600

    Added Navbar

--- web/assignments.php
+++ web/assignments.php
@@ -1,20 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <?php include './Partials/header.php';?>
+  <?php include './partials/header.php';?>
 </head>
 <body>
+  <?php include './partials/nav.php';?>
 
-<?php
-echo "My first PHP script!";
-?>
+
 
 <div class="container">
   <div class="row">
     <div class="col">
       <h1>Assignments</h1>
       <div class="list-group">
-        <a href="./01-Teach/index.php" class="list-group-item list-group-item-action">01 Teach</a>
+        <a href="./01-Teach/hello.html" class="list-group-item list-group-item-action">01 Teach</a>
         <a href="./02-Teach/index.html" class="list-group-item list-group-item-action">02 Teach</a>
       </div>
     </div>
@@ -22,7 +21,7 @@ echo "My first PHP script!";
 </div>
 
 
-<?php include './Partials/footer.php';?>
+<?php include './partials/footer.php';?>
 
 </body>
 </html>
---
+++ web/partials/nav.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">CS 313</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="https://mysterious-sands-95461.herokuapp.com/">Home</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Assignments
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="https://mysterious-sands-95461.herokuapp.com/01-Teach/hello.html">Week 01</a>
+          <a class="dropdown-item" href="https://mysterious-sands-95461.herokuapp.com/02-Teach/index.html">Week 02</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
