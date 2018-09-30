04443906a82c00963d27defc988ed05ef17a0aef
peterfoxflick <peterfoxflick@gmail.com>
2018-09-29 18:26:22 -0600

    Revised file struct

---
+++ index/assignments.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <?php
+  echo file_get_contents('./Partials/header.php');
+  ?>
+
+</head>
+<body>
+
+<?php
+echo "My first PHP script!";
+?>
+
+<h1>Guess whos back</h1>
+
+</body>
+</html>
