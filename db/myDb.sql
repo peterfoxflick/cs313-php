eb5edc5a632abc9be1721ad62819949a7ecb07bb
peterfoxflick <peterfoxflick@gmail.com>
2018-10-13 10:35:53 -0600

    Added Database

---
+++ db/myDb.sql
@@ -0,0 +1,78 @@
+/*  Learning Managment System
+ *  ---------
+ *  Users
+ *  Content
+ *  Courses
+ *  Completions
+*/
+
+
+/*  USER
+  ---------
+  user_id
+  user_name
+  password
+*/
+  CREATE TABLE "user" (
+   id         INT                    NOT NULL,
+   user_name  VARCHAR(20)            NOT NULL,
+   password   VARCHAR(20)            NOT NULL,
+   PRIMARY KEY( id )
+);
+
+INSERT INTO "user" VALUES (001, 'pflick', 'LetMeIn');
+INSERT INTO "user" VALUES (002, 'jimmyj', 'LetMeIn');
+INSERT INTO "user" VALUES (003, 'pointdext', 'LetMeIn');
+
+
+/*  Content
+--------------
+  id
+  name
+  data / text
+  coures_id
+  order
+*/
+  CREATE TABLE "content" (
+   id              INT                 NOT NULL,
+   name            VARCHAR(40)         NOT NULL,
+   data            TEXT                NOT NULL,
+   course_id       INT                 NOT NULL,
+   course_order    INT                 NOT NULL,
+   PRIMARY KEY( id ),
+   FOREIGN KEY (course_id) REFERENCES course(id)
+);
+INSERT INTO "content" VALUES (1, 'Select Plants', 'When selecting plants make sure to looks for...', 1, 1);
+INSERT INTO "content" VALUES (2, 'Finding Dirt', 'You will need to find a good chunk of dirt that...', 1, 2);
+INSERT INTO "content" VALUES (3, 'Planting', 'Place the seed into the dirt for it to start growing...', 1, 3);
+
+
+/*  Course
+--------------
+  id
+  name
+*/
+
+CREATE TABLE "course" (
+ id    INT                 NOT NULL,
+ name  VARCHAR(40)         NOT NULL,
+ PRIMARY KEY( id )
+);
+INSERT INTO "course" VALUES (1, 'Growing Plants');
+INSERT INTO "course" VALUES (2, 'Keeping Bees');
+INSERT INTO "course" VALUES (3, 'Arduinos for Beginners');
+
+
+/* Complete
+---------------
+  id
+  user_id
+  content_id
+*/
+CREATE TABLE "complete" (
+   id               INT       NOT NULL,
+   user_id          INT       NOT NULL,
+   content_id       INT       NOT NULL,
+   PRIMARY KEY( user_id )
+);
+INSERT INTO "complete" VALUES (1, 2, 2);
