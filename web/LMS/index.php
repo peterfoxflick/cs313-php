f9e791820a7f45824959bf6370d259610f4d5993
peterfoxflick <peterfoxflick@gmail.com>
2018-10-20 12:33:58 -0600

    Added course listing with database

--- web/LMS/index.php
+++ web/LMS/index.php
@@ -1,1 +1,50 @@
-// This will show all the possible coureses,
+<?php
+  function dbConnect(){
+    try {
+      $url = getenv('DATABASE_URL');
+      $opts = parse_url($url);
+      $server = $opts["host"];
+      $database = ltrim($opts["path"],'/');
+      $user = $opts["user"];
+      $password = $opts["pass"];
+      $port = $opts["port"];
+      $dsn = "pgsql:host={$server}; port={$port};dbname={$database}";
+      $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+      $link = new PDO($dsn, $user, $password, $options);
+      return $link;
+    }
+    catch (PDOException $ex) {
+      echo $ex;
+      exit;
+    }
+  }
+  function get_all_courses() {
+    $db = dbConnect();
+    $sql = "SELECT * FROM course";
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+    $data = $stmt->fetchAll(PDO::FETCH_NAMED);
+    $stmt->closeCursor();
+    return $data;
+  }
+  $courses = get_all_courses();
+?>
+
+
+
+
+<?php include './partials/header.php';?>
+
+<div class="container">
+  <div class="col">
+    <div class="row">
+      <ul class="list-group">
+        <?php foreach($course as $courses): ?>
+           <li class="list-group-item"><?= $course['name']; ?></li>
+         <?php endforeach; ?>
+      </ul>
+     </div>
+   </div>
+ </div>
+ 
+<?php include './partials/footer.php';?>
