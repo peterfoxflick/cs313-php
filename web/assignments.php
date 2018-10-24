46e56cb9798a8c9516a56d34aee0466bdc788cb2
peterfoxflick <peterfoxflick@gmail.com>
2018-10-24 13:05:49 -0600

    Updated assignments list

--- web/assignments.php
+++ web/assignments.php
@@ -14,6 +14,8 @@
           <a href="./01-Teach/hello.html" class="list-group-item list-group-item-action">01 Teach</a>
           <a href="./02-Teach/index.html" class="list-group-item list-group-item-action">02 Teach</a>
           <a href="./03-Teach/index.php" class="list-group-item list-group-item-action">03 Teach</a>
+          <a href="./05-Teach/index.php" class="list-group-item list-group-item-action">05 Teach</a>
+          <a href="./06-Teach/index.php" class="list-group-item list-group-item-action">06 Teach</a>
         </div>
         <h1>Proves</h1>
         <div class="list-group">
