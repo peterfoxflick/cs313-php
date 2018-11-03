04f216bbc4e834f67f836cd2098ce1513429048b
peterfoxflick <peterfoxflick@gmail.com>
2018-11-03 16:05:40 -0600

    get new order

--- web/LMS/editCourse.php
+++ web/LMS/editCourse.php
@@ -25,7 +25,7 @@ $name = get_course_data($course_id);
      <div class="row">
        <ul class="list-group" id="list">
          <?php foreach($contents as $content): ?>
-           <?php echo "<li><a href='./content.php?id={$content['id']}' class='list-group-item list-group-item-action'>{$content['name']}</a></li>" ?>
+           <?php echo "<li id='{$content['id']}'><a href='./content.php?id={$content['id']}' class='list-group-item list-group-item-action'>{$content['name']}</a></li>" ?>
           <?php endforeach; ?>
        </ul>
       </div>
@@ -58,11 +58,18 @@ $name = get_course_data($course_id);
  		 */
  		set: function (sortable) {
  			var order = sortable.toArray();
- 			localStorage.setItem(sortable.options.group.name, order.join('|'));
-      console.log(sortable.options.group.name + " +   +  " + order.join('|'));
+
  		}
  	}
  })
+
+
+ function save(){
+   var list = document.getElementById('list');
+   var ids = list.querySelectorAll('*[id]');
+   console.log(ids);
+
+ }
  </script>
 
 
