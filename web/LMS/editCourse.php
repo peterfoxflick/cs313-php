7a6aac6a120036204112443492573f4e29fac39a
peterfoxflick <peterfoxflick@gmail.com>
2018-11-03 16:29:00 -0600

    List order now saved on DB

--- web/LMS/editCourse.php
+++ web/LMS/editCourse.php
@@ -53,30 +53,7 @@ $name = get_course_data($course_id);
 
 
  var el = document.getElementById('list');
- Sortable.create(el, {
- 	group: "localStorage-example",
- 	store: {
- 		/**
- 		 * Get the order of elements. Called once during initialization.
- 		 * @param   {Sortable}  sortable
- 		 * @returns {Array}
- 		 */
- 		get: function (sortable) {
- 			var order = localStorage.getItem(sortable.options.group.name);
- 			return order ? order.split('|') : [];
- 		},
-
- 		/**
- 		 * Save the order of elements. Called onEnd (when the item is dropped).
- 		 * @param {Sortable}  sortable
- 		 */
- 		set: function (sortable) {
- 			var order = sortable.toArray();
- 			localStorage.setItem(sortable.options.group.name, order.join('|'));
-      save();
- 		}
- 	}
- })
+ Sortable.create(el);
  </script>
 
 
