46886b450d5fbd2fcb9cf0eba5a389b6701c788f
peterfoxflick <peterfoxflick@gmail.com>
2018-11-03 21:52:45 -0600

    Controller updated

--- web/LMS/controller/ediContent.php
+++ web/LMS/controller/ediContent.php
@@ -2,14 +2,15 @@
 <?php
 include '../partials/db.php';
 
-if(isset($_POST['nameIn']) && isset($_POST['dataIn']) && isset($_POST['courseIn']) && isset($_POST['orderIn'])) {
+if(isset($_POST['id']) && isset($_POST['nameIn']) && isset($_POST['dataIn']) && isset($_POST['courseIn']) && isset($_POST['orderIn'])) {
   $name = filter_var($_POST['nameIn'], FILTER_SANITIZE_STRING);
   $data = filter_var($_POST['dataIn'], FILTER_SANITIZE_STRING);
   $courseId = filter_var($_POST['courseIn'], FILTER_SANITIZE_NUMBER_INT);
   $orderId = filter_var($_POST['orderIn'], FILTER_SANITIZE_NUMBER_INT);
+  $id = filter_var($_POST['id'], FILTER_SANITIZE_NUMBER_INT);
 
 
-  add_content($name, $data, $courseId, $orderId);
+  edit_content($id, $name, $data, $courseId, $orderId);
 
   echo "<script type='text/javascript'>location.href = '../index.php';</script>";
 } else {
@@ -18,14 +19,15 @@ if(isset($_POST['nameIn']) && isset($_POST['dataIn']) && isset($_POST['courseIn'
 
 
 
-  function add_content($name, $data, $courseId, $orderId) {
+  function edit_content($id, $name, $data, $courseId, $orderId) {
     $db = dbConnect();
-    $sql = "INSERT INTO content ( name , data , course_id , course_order ) VALUES (:name, :data, :courseId, :order)";
+    $sql = "UPDATE content SET name = :name , data = :data , course_id :course_id , course_order = :course_order WHERE id = :id";
     $stmt = $db->prepare($sql);
     $stmt->bindValue(":name", $name, PDO::PARAM_STR);
     $stmt->bindValue(":data", $data, PDO::PARAM_STR);
     $stmt->bindValue(":courseId", $courseId, PDO::PARAM_INT);
     $stmt->bindValue(":order", $orderId, PDO::PARAM_INT);
+    $stmt->bindValue(":id", $id, PDO::PARAM_INT);
     $stmt->execute();
 
     $stmt->closeCursor();
